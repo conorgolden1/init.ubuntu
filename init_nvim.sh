ff33ac7bc177dd7ed093a46d3f9f19161e8a48aa
root <root@devlin.corp.criteriuminc.com>
2023-10-24 12:51:42 -0400

    Added init_nvim script

---
+++ init_nvim.sh
@@ -0,0 +1,61 @@
+apt update
+
+# Install Neovim
+
+nvim="~/.nvim/nvim.appimage"
+
+if [ ! -d "~/.nvim" ]; then
+   mkdir ~/.nvim
+   echo "Made .nvim directory at: ~/.nvim"
+fi
+
+echo "Downloading neovim : ~/.nvim/nvim.appimage"
+
+curl -o ~/.nvim/nvim.appimage -L https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x ~/.nvim/nvim.appimage
+
+cd ~/.nvim/ && { ./nvim.appimage --appimage-extract; cd -; }
+
+if [ -d "/etc/nvim" ]; then
+   rm -Rf /etc/nvim
+   echo "Removed old /etc/nvim"
+fi
+
+mv ~/.nvim/squashfs-root /etc/nvim
+
+if [ -h "/usr/bin/nvim" ]; then
+   rm /usr/bin/nvim
+   echo "Removed old /usr/bin/nvim symlink"
+fi
+
+ln -s /etc/nvim/AppRun /usr/bin/nvim
+
+# Clean up Neovim downloads
+
+rm -Rf ~/.nvim/
+
+
+# Download Neovim Config
+
+if [ -d "/etc/xdg/nvim" ]; then
+   rm -Rf /etc/xdg/nvim
+   echo "Removed old nvim config in /etc/xdg/nvim"
+fi
+
+git clone https://github.com/goldencm/init.lua.git /etc/xdg/nvim/
+
+# Install gcc
+
+apt install gcc g++-multilib -y
+
+# Install RipGrep
+
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
+dpkg -i ripgrep_13.0.0_amd64.deb
+rm ripgrep_13.0.0_amd64.deb
+
+
+# Run Packer Sync
+
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
