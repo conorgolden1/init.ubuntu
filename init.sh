78236b22c5465330ee23f33f317ff656875779f9
Conor Golden <conorgolden1@hotmail.com>
2023-11-22 11:15:22 -0500

    Added Node install

--- init.sh
+++ init.sh
@@ -108,3 +108,12 @@ gh auth login
 
 # Install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+#Install Node
+nvm install node
+nvm use node
+
